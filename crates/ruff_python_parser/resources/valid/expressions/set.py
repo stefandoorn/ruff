765a1d3a6bbc829c9ae761aaf8854c2954faeff3
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-25 08:38:20 +0530

    Add tests for set expression parsing

--- crates/ruff_python_parser/resources/valid/expressions/set.py
+++ crates/ruff_python_parser/resources/valid/expressions/set.py
@@ -1,3 +1,29 @@
+# Simple sets
+{}
+{1}
+{1,}
 {1, 2, 3}
-{1 + 2, (a, b), {1,2,3}, {a:b, **d}}
-{a}
+{1, 2, 3,}
+
+# Mixed with indentations
+{
+}
+{
+        1
+}
+{
+    1,
+        2,
+}
+
+# Nested
+{{1}}
+{{1, 2}, {3, 4}}
+
+# Named expression
+{x := 2}
+{1, x := 2, 3}
+{1, (x := 2),}
+
+# Random expressions
+{1 + 2, (a, b), {1, 2, 3}, {a: b, **d}}
